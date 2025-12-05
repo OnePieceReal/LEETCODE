775277b684199924cd166615f330b79bfd073c14
OnePieceReal <jihn.jose101@gmail.com>
2025-12-05 16:27:41 -0500

    testing

---
+++ leetcode/src/main/java/ArrayAndHashing/ContainsDuplicate.java
@@ -0,0 +1,15 @@
+package ArrayAndHashing;
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> map = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.contains(nums[i])){
+                return true;
+            }
+            map.add(nums[i]);
+        }
+        return false;
+    }
+}
